d06ac1b93b51d7c633a3d1d76a96d670895abe29
PankajJaiswar <pankajjaiswarpccs@gmail.com>
2025-12-19 16:28:52 +0530

    New Controller Added for DrillMastr

---
+++ src/wa_1235_jk_ecm_v4/Controllers/DrillMasterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using wa_1235_jk_ecm_v4.Interface;
+using wa_1235_jk_ecm_v4.Models;
+using wa_1235_jk_ecm_v4.Models.DecintellCommon;
+using static wa_1235_jk_ecm_v4.Models.Master;
+namespace wa_1235_jk_ecm_v4.Controllers
+{
+    public class DrillMasterController : Controller
+    {
+        private readonly IGenericMethods _iGenericMethods;
+        private readonly IAppSettingsService _appSettingsService;
+        public static DecintellSettings? appSettings;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DrillMasterController(IGenericMethods iGenericMethods, IAppSettingsService appSettingsService, IHttpContextAccessor httpContextAccessor)
+        {
+            _iGenericMethods = iGenericMethods;
+            _appSettingsService = appSettingsService;
+            appSettings = _appSettingsService.GetAppSettings();
+            _httpContextAccessor = httpContextAccessor;
+
+        }
+        private string JwtToken => _httpContextAccessor.HttpContext.Request.Cookies["1231_AccessToken"];
+
+
+
+    }
+}
